da4f5b1a3397a464312d8f20174f907c8e9a4d16
kevinanlipscomb <kevinandrewlipscomb@vt.edu>
2008-09-25 15:03:04 +0000

    Replaced CompareValidators with CustomValidators, whose behavior I can control better.

--- protected/change_password.pas
+++ protected/change_password.pas
@@ -25,6 +25,8 @@ type
     procedure TWebForm_change_password_PreRender(sender: System.Object;
       e: System.EventArgs);
     procedure Button_cancel_Click(sender: System.Object; e: System.EventArgs);
+    procedure CustomValidator_confirmation_password_ServerValidate(source: System.Object; 
+      args: System.Web.UI.WebControls.ServerValidateEventArgs);
   {$ENDREGION}
   strict private
     p: p_type;
@@ -36,10 +38,9 @@ type
     TextBox_confirmation_password: System.Web.UI.WebControls.TextBox;
     RequiredFieldValidator_nominal_password: System.Web.UI.WebControls.RequiredFieldValidator;
     RequiredFieldValidator_confirmation_password: System.Web.UI.WebControls.RequiredFieldValidator;
-    CompareValidator1: System.Web.UI.WebControls.CompareValidator;
     RegularExpressionValidator_password: System.Web.UI.WebControls.RegularExpressionValidator;
-    CompareValidator_confirmation_password: System.Web.UI.WebControls.CompareValidator;
     Button_cancel: System.Web.UI.WebControls.Button;
+    CustomValidator_confirmation_password: System.Web.UI.WebControls.CustomValidator;
   protected
     procedure OnInit(e: EventArgs); override;
   private
@@ -57,6 +58,7 @@ implementation
 /// </summary>
 procedure TWebForm_change_password.InitializeComponent;
 begin
+  Include(Self.CustomValidator_confirmation_password.ServerValidate, Self.CustomValidator_confirmation_password_ServerValidate);
   Include(Self.Button_submit.Click, Self.Button_submit_Click);
   Include(Self.Button_cancel.Click, Self.Button_cancel_Click);
   Include(Self.PreRender, Self.TWebForm_change_password_PreRender);
@@ -92,6 +94,12 @@ begin
   inherited OnInit(e);
 end;
 
+procedure TWebForm_change_password.CustomValidator_confirmation_password_ServerValidate(source: System.Object;
+  args: System.Web.UI.WebControls.ServerValidateEventArgs);
+begin
+  args.isvalid := (TextBox_nominal_password.text.trim = TextBox_confirmation_password.text.trim);
+end;
+
 procedure TWebForm_change_password.Button_cancel_Click(sender: System.Object;
   e: System.EventArgs);
 begin
@@ -107,8 +115,12 @@ end;
 procedure TWebForm_change_password.Button_submit_Click(sender: System.Object;
   e: System.EventArgs);
 begin
-  p.biz_users.SetPassword(p.biz_user.IdNum,Digest(Safe(TextBox_nominal_password.Text.trim,ALPHANUM)));
-  BackTrack;
+  if page.isvalid then begin
+    p.biz_users.SetPassword(p.biz_user.IdNum,Digest(Safe(TextBox_nominal_password.Text.trim,ALPHANUM)));
+    BackTrack;
+  end else begin
+    ValidationAlert(TRUE);
+  end;
 end;
 
 end.
