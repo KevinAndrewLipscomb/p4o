4f5d0d3f22ae4058b0c890c3f2066ba157014ebb
KevinAnLipscomb <kevinandrewlipscomb@vt.edu>
2009-08-18 21:19:59 +0000

    Added command to open corresponding aplus.net folder.

--- work.bat
+++ work.bat
@@ -4,5 +4,6 @@ REM
 cd "C:\Inetpub\wwwroot\p4o"
 start /max explorer /e,/select,C:\Inetpub\wwwroot\p4o\.svn
 start /max explorer ftp://ftp.frompaper2web.com/ssl/p4o_x
+start /max explorer ftp://frompaper2web.win.aplus.net/WWWROOT/p4o/
 start /max p4o.dproj
 start "" /max "C:\Program Files\MySQL\MySQL Tools for 5.0\MySQLQueryBrowser.exe"
