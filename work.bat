74acd3db8dafc2a5c09dc5706afe366e0c63c1a7
KevinAnLipscomb <kevinandrewlipscomb@vt.edu>
2009-09-22 19:56:21 +0000

    Revised to open SLN file instead of DPROJ file.

--- work.bat
+++ work.bat
@@ -5,5 +5,5 @@ cd "C:\Inetpub\wwwroot\p4o"
 start /max explorer /e,/select,C:\Inetpub\wwwroot\p4o\.svn
 start /max explorer ftp://ftp.frompaper2web.com/ssl/p4o_x
 start /max explorer ftp://frompaper2web.win.aplus.net/WWWROOT/p4o/
-start /max p4o.dproj
+start /max p4o.sln
 start "" /max "C:\Program Files\MySQL\MySQL Tools for 5.0\MySQLQueryBrowser.exe"
