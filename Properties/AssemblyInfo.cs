6b1e2beb9b1257cf12d8acaea37da45739ee3b33
KevinAnLipscomb <kevinandrewlipscomb@vt.edu>
2009-09-24 00:14:00 +0000

    Corrected AssemblyInfo settings.

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -8,9 +8,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("p4o")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("IBM")]
+[assembly: AssemblyCompany("Kalips'o Infogistics LLC")]
 [assembly: AssemblyProduct("p4o")]
-[assembly: AssemblyCopyright("Copyright © IBM 2009")]
+[assembly: AssemblyCopyright("Copyright © Kalips'o Infogistics LLC 2009")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
