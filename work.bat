fa957af87688b6148023a9e91485fc77a4db8b16
KevinAnLipscomb <kevinandrewlipscomb@vt.edu>
2009-10-04 18:55:22 +0000

    Revised to use Filezilla since IE8 FTP has credential-saving issues.

--- work.bat
+++ work.bat
@@ -3,7 +3,7 @@ REM $Id$
 REM
 cd "C:\Inetpub\wwwroot\p4o"
 start /max explorer /e,/select,C:\Inetpub\wwwroot\p4o\.svn
-start /max explorer ftp://ftp.frompaper2web.com/ssl/p4o_x
-start /max explorer ftp://frompaper2web.win.aplus.net/WWWROOT/p4o/
+start "" /max "C:\Program Files\FileZilla FTP Client\filezilla.exe" --site=0/ftp.frompaper2web.com\/ssl/p4o_x
+start "" /max "C:\Program Files\FileZilla FTP Client\filezilla.exe" --site=0/frompaper2web.win.aplus.net\/WWWROOT/p4o/
 start /max p4o.sln
 start "" /max "C:\Program Files\MySQL\MySQL Tools for 5.0\MySQLQueryBrowser.exe"
