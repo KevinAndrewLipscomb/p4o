9605a87a6c02ba7f7e03b56c0e96a2ac6f26d519
KevinAnLipscomb <KevinAnLipscomb@fcafd3cc-6610-0410-92fd-86a883f3e63b>
2010-05-26 20:07:48 +0000

    Revised to use built-in FTP instead of WinSCP.

--- trunk/p4o/work.bat
+++ trunk/p4o/work.bat
@@ -3,7 +3,7 @@ REM $Id$
 REM
 cd "C:\Inetpub\wwwroot\p4o"
 start /max explorer /e,/select,C:\Inetpub\wwwroot\p4o\.svn
-start "" /max "C:\Program Files\WinSCP\WinSCP.exe" alentus.com/ssl/p4o_x
-start "" /max "C:\Program Files\WinSCP\WinSCP.exe" aplus.net/p4o/
+start /max explorer ftp://kalipso:@frompaper2web.com/ssl/p4o_x
+start /max explorer ftp://frompaper2web:@frompaper2web.win.aplus.net/WWWROOT/p4o/
 start /max p4o.sln
 start "" /max "C:\Program Files\MySQL\MySQL Workbench 5.2 OSS\MySQLWorkbench.exe"
