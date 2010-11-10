5fb1e59ee615ec1427f3065748203cbe0270aef5
KevinAnLipscomb <KevinAnLipscomb@fcafd3cc-6610-0410-92fd-86a883f3e63b>
2010-11-10 18:23:35 +0000

    Removed reference to a defunct server account.  Updated MySql.Data.dll.  Corrected connection strings.

--- trunk/p4o/work.bat
+++ trunk/p4o/work.bat
@@ -4,6 +4,5 @@ REM
 cd "C:\Inetpub\wwwroot\p4o"
 start /max explorer /e,/select,C:\Inetpub\wwwroot\p4o\.svn
 start /max explorer ftp://kalipso:@frompaper2web.com/ssl/p4o_x
-start /max explorer ftp://frompaper2web:@frompaper2web.win.aplus.net/WWWROOT/p4o/
 start /max p4o.sln
-start "" /max "C:\Program Files\MySQL\MySQL Workbench 5.2 OSS\MySQLWorkbench.exe"
+start "" /max "C:\Program Files\MySQL\MySQL Workbench 5.2 CE\MySQLWorkbench.exe"
