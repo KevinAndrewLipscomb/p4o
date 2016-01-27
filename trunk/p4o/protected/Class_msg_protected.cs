0da909d7b3b69634d5bcb1205ef2d8670c3c623e
kevinanlipscomb <kevinanlipscomb@fcafd3cc-6610-0410-92fd-86a883f3e63b>
2016-01-27 22:17:46 +0000

    Added role_member_map_log feature.

---
+++ trunk/p4o/protected/Class_msg_protected.cs
@@ -0,0 +1,17 @@
+using kix;
+using System;
+
+namespace Class_msg_protected
+  {
+  public class TClass_msg_protected
+    {
+    //public class 0_detail
+    //  {
+    //  internal object summary = null;
+    //  }
+    public class overview
+      {
+      internal string target = k.EMPTY;
+      }
+    }
+  }
