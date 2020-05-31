62d9eeca5075f63cdf26840f6835537d8c22212d
kevinanlipscomb <kevinanlipscomb@fcafd3cc-6610-0410-92fd-86a883f3e63b>
2020-05-31 21:15:29 +0000

    Removed attempt to open FTP site.

--- trunk/p4o/work.bat
+++ trunk/p4o/work.bat
@@ -3,6 +3,5 @@ REM $Id$
 REM
 cd "C:\Inetpub\wwwroot\p4o"
 start /max explorer /e,/select,C:\Inetpub\wwwroot\p4o\.svn
-start /max explorer ftp://kalipso:@frompaper2web.com/ssl/p4o_x
 start /max p4o.sln
 IF EXIST "C:\Program Files\MySQL\MySQL Workbench\MySQLWorkbench.exe" (start "" /max "C:\Program Files\MySQL\MySQL Workbench\MySQLWorkbench.exe") ELSE start "" /max "C:\Program Files (x86)\MySQL\MySQL Workbench\MySQLWorkbench.exe"
